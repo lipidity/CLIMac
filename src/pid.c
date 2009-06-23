1c7ca43173a0ee23b6bdbdb76e5ce0704f75e9e6
Ankur Kothari <ankur@lipidity.com>
2009-06-23 23:24:17 +1000

    pid manpage, option -l for all PIDs with name

--- src/pid.c
+++ src/pid.c
@@ -4,26 +4,37 @@
 #import <errno.h>
 
 int main (int argc, const char * argv[]) {
-	if (argc == 2) {
+	if (argc > 1) {
 		size_t len = 0;
 		struct kinfo_proc *r = NULL;
 
 		int name[] = { CTL_KERN, KERN_PROC, KERN_PROC_ALL, 0 };
-		if (sysctl(name, 3, NULL, &len, NULL, 0) || ((r = malloc(len)) == NULL) || sysctl(name, 3, r, &len, NULL, 0)) {
-			err(1, NULL);
-		}
+		if (sysctl(name, 3, NULL, &len, NULL, 0) || ((r = malloc(len)) == NULL) || sysctl(name, 3, r, &len, NULL, 0))
+			err(2, NULL);
 
 		size_t c = len / sizeof(struct kinfo_proc);
 
+		struct kinfo_proc *k = r;
 		if (strcmp(argv[1], "-l") == 0) {
-	//		puts("    PID Process");
-			struct kinfo_proc *k = r;
-			while (c--) {
-				printf("%7d %s\n", k->kp_proc.p_pid, k->kp_proc.p_comm);
-				k++;
+			if (argc == 2) {
+//				puts("    PID Process");
+				while (c--) {
+					printf("%8d %s\n", k->kp_proc.p_pid, k->kp_proc.p_comm);
+					k++;
+				}
+				return 0;
+			} else if (argc == 3) {
+				int retval = 1;
+				while (c--) {
+					if (strcmp(k->kp_proc.p_comm, argv[2]) == 0) {
+						printf("%d\n", k->kp_proc.p_pid);
+						retval = 0;
+					}
+					k++;
+				}
+				return retval;
 			}
-		} else {
-			struct kinfo_proc *k = r;
+		} else if (argc == 2) {
 			while (c--) {
 				if (strcmp(k->kp_proc.p_comm, argv[1]) == 0) {
 					printf("%d\n", k->kp_proc.p_pid);
@@ -31,12 +42,10 @@ int main (int argc, const char * argv[]) {
 				}
 				k++;
 			}
-			errx(2, "'%s': Process not found\n", argv[1]);
+			return 1;
 		}
-		return 0;
-	} else {
-		fprintf(stderr, "usage:  %s <process-name>\n"
-				"\t%s -l\n", argv[0], argv[0]);
-		return 1;
 	}
+	fprintf(stderr, "usage:  %s <process-name>\n"
+			"\t%s -l [<process-name>]\n", argv[0], argv[0]);
+	return 1;
 }
