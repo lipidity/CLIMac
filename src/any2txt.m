0d4c9ad99689d5828bef368f92af8b036c4f0aa0
Ankur Kothari <ankur@lipidity.com>
2009-06-24 16:42:49 +1000

    any2txt: check quicklook first any2txt: coerce webcontent UTI to HTML attributed string

--- src/any2txt.m
+++ src/any2txt.m
@@ -3,59 +3,74 @@
 #import <QuickLook/QuickLook.h>
 
 typedef void *QLPreviewRef;
-extern QLPreviewRef QLPreviewCreate(void *unknownNULL, CFURLRef fileURL, CFDictionaryRef options);
+extern QLPreviewRef QLPreviewCreate(void *unknownNULL, CFTypeRef item, CFDictionaryRef options);
 extern CFDataRef QLPreviewCopyData(QLPreviewRef thumbnail);
 extern CFURLRef QLPreviewCopyURLRepresentation(QLPreviewRef);
 extern CFDictionaryRef QLPreviewCopyOptions(QLPreviewRef);
 extern CFDictionaryRef QLPreviewCopyProperties(QLPreviewRef);
-extern CFTypeRef QLPreviewGetPreviewType(QLPreviewRef); // eg. public.webcontent; public.text; public.image; public.pdf
+extern CFStringRef QLPreviewGetPreviewType(QLPreviewRef); // eg. public.webcontent; public.text; public.image; public.pdf
 extern void QLPreviewSetPreviewType(QLPreviewRef, CFStringRef);
 //extern void QLPreviewSetForceContentTypeUTI(QLPreviewRef, CFStringRef);
 
 int main (int argc, char *argv[]) {
 	if (argc == 2 || argc == 3) {
 		NSAutoreleasePool *pool = [NSAutoreleasePool new];
-		CFURLRef inURL = CFURLCreateFromFileSystemRepresentation(NULL, (UInt8 *)argv[1], strlen(argv[1]), false);
-		if (inURL != NULL) {
-			NSError *error = [NSError errorWithDomain:NSPOSIXErrorDomain code:EINVAL userInfo:nil];
-			NSData *data = nil;
-			id str = [[NSAttributedString alloc] initWithURL:(NSURL *)inURL options:nil documentAttributes:NULL error:&error];
-			if (str == nil)
-				str = [[PDFDocument alloc] initWithURL:(NSURL *)inURL];
-			if (str == nil && &QLPreviewCreate != NULL) {
-				QLPreviewRef p = QLPreviewCreate(NULL, inURL, NULL);
-				if (p != NULL) {
-					if (UTTypeConformsTo(QLPreviewGetPreviewType(p), kUTTypeText)) {
-						NSData *rtf = (NSData *)QLPreviewCopyData(p);
-						NSDictionary *prop = (NSDictionary *)QLPreviewCopyProperties(p);
-						str = [[NSAttributedString alloc] initWithData:rtf options:prop documentAttributes:NULL error:&error];
+		id input = nil;
+		id str = nil;
+		NSError *error = [NSError errorWithDomain:NSPOSIXErrorDomain code:EINVAL userInfo:nil];
+		NSData *data = nil;
+		if (strcmp(argv[1], "-") == 0)
+			input = [[[NSFileHandle fileHandleWithStandardInput] readDataToEndOfFile] retain];
+		else
+			input = (NSURL *)CFURLCreateFromFileSystemRepresentation(NULL, (UInt8 *)argv[1], strlen(argv[1]), false);
+		if (str == nil && &QLPreviewCreate != NULL) {
+			QLPreviewRef p = QLPreviewCreate(NULL, (CFTypeRef)input, NULL);
+			if (p != NULL) {
+				QLPreviewSetPreviewType(p, kUTTypeText);
+				CFStringRef type = QLPreviewGetPreviewType(p);
+				if (UTTypeConformsTo(type, kUTTypeText) || [@"public.webcontent" isEqualToString:(NSString *)type]) {
+					NSData *rtf = (NSData *)QLPreviewCopyData(p);
+					NSDictionary *prop = (NSDictionary *)QLPreviewCopyProperties(p) ? : [[NSDictionary alloc] init];
+					if ([@"public.webcontent" isEqualToString:(NSString *)type]) {
+						NSMutableDictionary *newProp = [prop mutableCopy];
+						[newProp setObject:NSHTMLTextDocumentType forKey:NSDocumentTypeDocumentOption];
+						[prop release];
+						prop = newProp;
+					}
+					str = [[NSAttributedString alloc] initWithData:rtf options:prop documentAttributes:NULL error:&error];
+					if (prop)
 						CFRelease(prop);
-						[rtf release];
-					}
-					CFRelease(p);
+					[rtf release];
 				}
+				CFRelease(p);
 			}
-			CFRelease(inURL);
-			if (str != nil) {
-				data = [[str string] dataUsingEncoding:NSUTF8StringEncoding allowLossyConversion:YES];
-				[str release];
-			}
-			if (data != nil) {
-				if (argc == 3) {
-					CFStringRef outPath = CFStringCreateWithFileSystemRepresentation(NULL, argv[2]);
-					if (outPath && [data writeToFile:(NSString *)outPath atomically:NO]) {
-						CFRelease(outPath);
-						return 0;
-					}
-				} else {
-					if (fwrite([data bytes], [data length], 1, stdout) == 1)
-						return 0;
-				}
-				err(1, NULL);
-			}
-			fputs([[error localizedFailureReason] fileSystemRepresentation], stderr);
-			putchar('\n');
 		}
+		if (str == nil) {
+			if ([input isKindOfClass:[NSURL class]])
+				str = [[NSAttributedString alloc] initWithURL:input options:nil documentAttributes:NULL error:&error] ? : [[PDFDocument alloc] initWithURL:input];
+			else
+				str = [[NSAttributedString alloc] initWithData:input options:nil documentAttributes:NULL error:&error] ? : [[PDFDocument alloc] initWithData:input];
+		}
+		[input release];
+		if (str != nil) {
+			data = [[str string] dataUsingEncoding:NSUTF8StringEncoding];
+			[str release];
+		}
+		if (data != nil) {
+			if (argc == 3) {
+				CFStringRef outPath = CFStringCreateWithFileSystemRepresentation(NULL, argv[2]);
+				if (outPath && [data writeToFile:(NSString *)outPath atomically:NO]) {
+					CFRelease(outPath);
+					return 0;
+				}
+			} else {
+				if (fwrite([data bytes], [data length], 1, stdout) == 1)
+					return 0;
+			}
+			err(1, NULL);
+		}
+		fputs([[error localizedFailureReason] fileSystemRepresentation], stderr);
+		putchar('\n');
 		[pool release];
 		return 1;
 	}
