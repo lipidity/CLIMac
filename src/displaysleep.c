69b93bc1a4ef1505ff03e48351e6dc314ca49e80
Ankur Kothari <ankur@lipidity.com>
2009-06-25 01:51:44 +1000

    add displaysleep

---
+++ src/displaysleep.c
@@ -0,0 +1,41 @@
+#import <IOKit/IOKitLib.h>
+#if 1
+int main (int argc, char *argv[]) {
+	io_registry_entry_t io = IORegistryEntryFromPath(kIOMasterPortDefault, kIOServicePlane ":/IOResources/IODisplayWrangler");
+	if (io != MACH_PORT_NULL) {
+		usleep(300000);
+		IORegistryEntrySetCFProperty(io, CFSTR("IORequestIdle"), kCFBooleanTrue);
+		IOObjectRelease(io);
+		return 0;
+	}
+	return 1;
+}
+#else
+int
+main(int argc, char **argv)
+{
+    kern_return_t kr;
+    CFTypeRef obj;
+	
+    io_registry_entry_t regEntry;
+	
+    regEntry = IORegistryEntryFromPath(kIOMasterPortDefault, 
+									   kIOServicePlane ":/IOResources/IODisplayWrangler");
+	
+    obj = CFRetain(kCFBooleanTrue);
+    if (argc > 1)
+    {
+		SInt32 num = 1000 * strtol(argv[1], 0, 0);
+		obj = CFNumberCreate(kCFAllocatorDefault, kCFNumberSInt32Type, &num);
+    }
+	
+    kr = IORegistryEntrySetCFProperty(regEntry, CFSTR("IORequestIdle"), obj);
+	
+    printf("IORegistryEntrySetCFProperty(IORequestIdle) 0x%x\n", kr);
+	
+    CFRelease(obj);
+    IOObjectRelease(regEntry);
+	
+    return (0);
+}
+#endif
