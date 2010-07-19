17edf4fc122f7707e4b64d6fd845a9d92125d0fd
Ankur Kothari <ankz.kothari@gmail.com>
2010-07-19 15:25:14 +1000

    write dict utilitiy
    using only public API, show definitions of a word

--- src/dict.c
+++ src/dict.c
@@ -1,31 +1,66 @@
 /*
- * Look up words in dictionary
+ * Look up words in system dictionaries
+ *
+ * Copyright (C) Vacuous Virtuoso
+ * <http://lipidity.com/climac/>
  */
 
-#include "version.h"
-#include "ret_codes.h"
-
-#import <Carbon/Carbon.h>
 #import <CoreServices/CoreServices.h>
 
-#include <stdio.h>
-#include <sys/stat.h>
+#import "climac.h"
 
-int main(int argc, char *argv[]) 
-{
-	if (argc == 1) {
-		printf("");
-		pclose(NULL);
-		struct stat st;
-		st.
+static void usage(FILE *);
+
+static const struct option longopts[] = {
+	{ "help", no_argument, NULL, 'h' },
+	{ "version", no_argument, NULL, 'V' },
+
+	{ NULL, 0, NULL, 0 }
+};
+
+int main(int argc, char *argv[]) {
+	int c;
+	while ((c = getopt_long(argc, argv, "hV", longopts, NULL)) != EOF) {
+		switch (c) {
+			case 'V':
+				climac_version_info();
+				exit(RET_SUCCESS);
+			case 'h':
+				usage(stdout);
+				exit(RET_USAGE);
+			default:
+				usage(stderr);
+				exit(RET_USAGE);
+		}
 	}
-struct 
-struct stat st;
-st = {0};
-return 0;
+
+	argc -= optind;
+	argv += optind;
+
+	if (argc != 1) {
+		usage(stderr);
+		exit(RET_USAGE);
+	}
+
+	// following objects are leaked
+
+	CFStringRef word = CFStringCreateWithFileSystemRepresentation(NULL, argv[0]);
+	if (word == NULL)
+		errx(RET_ERROR, "bad string");
+	CFRange range = CFRangeMake(0, CFStringGetLength(word));
+
+	CFStringRef def = DCSCopyTextDefinition(NULL, word, range);
+	if (def != NULL) {
+		CFIndex n = CFStringGetMaximumSizeOfFileSystemRepresentation(def);
+		char *str = xmalloc(n);
+		if (CFStringGetFileSystemRepresentation(def, str, n) == true) {
+			puts(str);
+			exit(RET_SUCCESS);
+		}
+	}
+	exit(RET_FAILURE);
 }
 
-int add(int a, int b) 
-{
-	return a + b;
+static void usage(FILE *outfile) {
+	fprintf(outfile, "Usage: %s <word>\n", getprogname());
 }
